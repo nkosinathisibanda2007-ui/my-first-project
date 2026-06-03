2766dcc8b09c2dbc3618951e373f13f5d684c6a5
nkosinathisibanda2007-ui <nkosinathisibanda2007@gmail.com>
2026-06-04 01:40:27 +0200

    Added a calculator script

---
+++ calc.py
@@ -0,0 +1,6 @@
+print("--- Termux Calculator ---")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+result = num1 + num2
+print(f"The total is: {result}")
+
